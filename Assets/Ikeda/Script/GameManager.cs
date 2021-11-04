5f39c3cdaf744c5ea098123272cf53a2860c26c8
kaeru411r <ike269fr@gmail.com>
2021-11-04 12:05:03 +0900

    ポーズ用デリゲートを用意

---
+++ Assets/Ikeda/Script/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public static Action _OnPause;
+    public static Action _OnResume;
+    bool _pause;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (_pause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Pause()
+    {
+        _OnPause();
+        _pause = true;
+    }
+
+    void Resume()
+    {
+        _OnResume();
+        _pause = false;
+    }
+}
