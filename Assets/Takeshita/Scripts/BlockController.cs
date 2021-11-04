bc9d1f4aba91975bccbb18e608efa6f4bfabb8bd
ChihiroTakeshita <83819925+ChihiroTakeshita@users.noreply.github.com>
2021-11-04 16:00:06 +0900

    プレイヤーのプレハブを作成、ポーズに対応

---
+++ Assets/Takeshita/Scripts/BlockController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    public bool isLast = true;
+    private Rigidbody2D _rb;
+    private Vector3 tempV;
+
+    private void OnEnable()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        GameManager._OnPause += OnPause;
+        GameManager._OnResume += OnResume;
+    }
+
+    private void OnDisable()
+    {
+        GameManager._OnPause -= OnPause;
+        GameManager._OnResume -= OnResume;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag != "Player" && isLast)
+        {
+            this.transform.position += new Vector3(0, 1, 0);
+        }
+    }
+
+    public void OnPause()
+    {
+        tempV = _rb.velocity;
+        _rb.velocity = Vector3.zero;
+    }
+
+    public void OnResume()
+    {
+        _rb.velocity = tempV;
+    }
+}
---
+++ Assets/Takeshita/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed = 1f;
+    [SerializeField] float _jumpSpeed = 1f;
+
+    BlockGenerator _generator;
+    Rigidbody2D _rb;
+    Vector3 _defaultScale;
+    Vector2 _jumpForce;
+    bool _isFlip;
+    bool _isGrounded;
+
+    bool _isPause = false;
+    Vector3 _tempV = Vector3.zero;
+
+    private void Awake()
+    {
+        _generator = GetComponent<BlockGenerator>();
+        _rb = GetComponent<Rigidbody2D>();
+        _defaultScale = this.transform.localScale;
+        _jumpForce = new Vector2(0, _jumpSpeed);
+    }
+
+    private void OnEnable()
+    {
+        GameManager._OnPause += OnPause;
+        GameManager._OnResume += OnResume;
+    }
+
+    private void OnDisable()
+    {
+        GameManager._OnPause -= OnPause;
+        GameManager._OnResume -= OnResume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!_isPause)
+        {
+            var h = Input.GetAxisRaw("Horizontal");
+            var velocityY = _rb.velocity.y;
+            if (_isFlip && h > 0)
+            {
+                this.transform.localScale = _defaultScale;
+                _isFlip = !_isFlip;
+            }
+            else if (!_isFlip && h < 0)
+            {
+                this.transform.localScale = new Vector3(_defaultScale.x * -1, _defaultScale.y, _defaultScale.z);
+                _isFlip = !_isFlip;
+            }
+            _rb.velocity = new Vector3(h * _moveSpeed, velocityY, 0);
+
+            if (Input.GetButtonDown("Jump") && _isGrounded)
+            {
+                _rb.AddForce(_jumpForce, ForceMode2D.Impulse);
+            }
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                _generator.SpawnBlock(transform.position, _isFlip);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _isGrounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _isGrounded = false;
+    }
+
+    public void OnPause()
+    {
+        _isPause = true;
+        _tempV = _rb.velocity;
+        _rb.velocity = Vector3.zero;
+    }
+
+    public void OnResume()
+    {
+        _isPause = false;
+        _rb.velocity = _tempV;
+    }
+}
