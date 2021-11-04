981350f379382e4c514f9cc248337791d2ec6306
MutoAyumu <mutoayum0610@gmail.com>
2021-11-04 13:44:16 +0900

    ダメージブロックの作成

---
+++ Assets/Muto/Script/DamageFloor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageFloor : MonoBehaviour
+{
+    [SerializeField] string _playerTag = "Player";
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag(_playerTag))
+        {
+            var player = collision.gameObject.GetComponent<PlayerLifeManager>();
+            player.Damage();
+        }
+    }
+}
