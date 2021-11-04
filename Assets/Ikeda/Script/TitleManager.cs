c0a151ee1da91988d0d14715ef5de258a1a9ead5
kaeru411r <ike269fr@gmail.com>
2021-11-04 11:49:14 +0900

    タイトルから遷移できるように

---
+++ Assets/Ikeda/Script/TitleManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] Scene _gameScene;
+
+    public void SceneChange()
+    {
+        SceneManager.LoadScene(_gameScene.handle);
+    }
+}
